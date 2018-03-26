a5a93d20864213f63ef34a1c456c9258388d672d
Daniel Brown <derbraun@gmail.com>
2018-03-26 16:55:41 -0600

    removing the router and going back to app calls

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 //Setting app to use express
 var app = express();
@@ -28,8 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,6 +46,66 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//gets for server
+
+
+let grades = [];
+let id = 0;
+
+
+app.get('/grades', (req, res) => {
+    console.log("router get called");
+    res.send(grades);
+});
+
+app.post('/grades', (req, res) => {
+    id++;
+    let grade = {id:id, name:req.body.name, letter:req.body.grade};
+    grades.push(grade);
+    res.send(grade);
+});
+
+
+app.put('/grades/:id', (req,res) =>{
+    //Grab id from request and connect it to its grade
+    let id = parseInt(req.params.id);
+    let gradesMap = grades.map(grade => {return grade.id});
+    let index = gradesMap.indexOf(id);
+    let grade = grades[index];
+
+    //setting grade values
+    grade.name = req.body.name;
+    grade.letter = req.body.letter;
+
+    //handle drag and drop re-ordering
+    if(req.body.orderChange){
+        let indexTarget = gradesMap.indexOf(req.body.orderTarget);
+        grades.splice(index,1);
+        grades.splice(indexTarget,0,grade);
+    }
+
+    res.send(grade);
+});
+
+app.delete('/grades/:id', (req, res) => {
+    //connect id to location in grades
+    let id = parseInt(req.params.id);
+    let removeIndex = grades.map(grade => {return grade.id; }).indexOf(id);
+
+    if(removeIndex === -1){
+        res.status(404).send("Sorry that record doesn't exist");
+        return;
+    }
+
+    //remove from list
+    grades.splice(removeIndex,1);
+    res.sendStatus(200);
+});
+
+
+module.exports = router;
+
 
 app.listen(8080, () => console.log('Creative Server listening on port 8080'));
 
