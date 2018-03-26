7e8cf08936758ba96e310e416508dd480ec6536c
Daniel Brown <derbraun@gmail.com>
2018-03-26 17:06:14 -0600

    calls work, put still only getting a blank screen. Added some debugging code.

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 "use strict";
 
-
-//var createError = require('http-errors');
 var express = require('express');
-//var path = require('path');
-//var cookieParser = require('cookie-parser');
-//var logger = require('morgan');
 var bodyParser = require('body-parser');
-
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
 
 //Setting app to use express
 var app = express();
@@ -19,32 +11,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 
-//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static('public'));
 
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-//gets for server
+//calls for server
 let grades = [];
 let id = 0;
 
